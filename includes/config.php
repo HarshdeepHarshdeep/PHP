2459119453c5bb1c69167a4cef3274a4a79091b1
HarshdeepHarshdeep <100747330+HarshdeepHarshdeep@users.noreply.github.com>
2022-10-05 23:55:03 -0400

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+  // Define the database connection constants here!
+ define('DB_HOST', 'localhost');
+ define('DB_USER', 'hharshdeep');     
+ define('DB_PASSWORD', '3pxk53pxk5vtkz4vtkz4');  
+ define('DB_NAME', 'hharshdeepmedia');   
+
+  //Add your blog name below
+  $config_blogname = "The Best Blog in the Universe";
+
+  //Add your name below
+  $config_author = "Nicholas Sylvestre";
+
+
+
+
+?>
+
